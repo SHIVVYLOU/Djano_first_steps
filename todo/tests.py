128eff4955873d55469f353e32f9138e0b482ce9
EC2 Default User <ec2-user@ip-172-31-40-202.ec2.internal>
2019-08-19 09:48:27 +0000

    test files created

--- todo/tests.py
+++ todo/tests.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 # Create your tests here.
+class TestDjango(TestCase):
+    
+    def test_is_this_thing_on(self):
+        self.assertEqual(1, 1)
